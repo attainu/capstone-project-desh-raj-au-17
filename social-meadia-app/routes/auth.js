2f0c624d02a9fbe91a5866ad8e029f14ff8b2594
Vaishnavi Gupta <vaishnaviguptamakeup@gmail.com>
2022-01-25 20:55:34 +0530

    friendbook

--- social-meadia-app/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { registerUser, userLogin } = require("../controllers/authControllers");
-
-router.route("/register").post(registerUser);
-router.route("/login").post(userLogin);
-
-module.exports = router;
--- social-meadia-app/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/user", (req, res) => {
-  res.send(`welcom to home page`);
-});
-
-module.exports = router;

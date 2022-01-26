ff83b149c4a4aef5662c1e7d5c222bbcaa076822
Vaishnavi Gupta <vaishnaviguptamakeup@gmail.com>
2022-01-26 20:08:11 +0530

    backend changes

--- FriendBook Project/backend/index.js
+++ FriendBook Project/backend/index.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute = require("./backend/routes/users");
-const authRoute = require("./backend/routes/auth");
-const postRoute = require("./backend/routes/post");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
 
 dotenv.config();
 
